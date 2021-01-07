104691922322248780936a9fd90c3c9546dbe5af
OscarClemente <oscar.clemente@gmail.com>
2021-01-07 15:05:10 +0100

    Chapter 13 added closures.

---
+++ 13-iterators_and_closures/hash_map_cacher_closure/src/main.rs
@@ -0,0 +1,74 @@
+use std::thread;
+use std::time::Duration;
+use std::collections::HashMap;
+
+fn main() {
+    generate_workout(25, 8);
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
+
+// This cacher struct contains a closure that will only be executed once
+// The value will be saved and from then on, the closure won't be executed
+// the value will be instantly returned
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    values: HashMap<u32, u32>, // Value is cached
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            values: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        let value = self.values.get(&arg);
+        match value {
+            Some(v) => *v, // closure is not executed anymore
+            None => {
+                let v = (self.calculation)(arg);
+                self.values.insert(arg, v);
+                v
+            }
+        }
+    }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 2);
+}
