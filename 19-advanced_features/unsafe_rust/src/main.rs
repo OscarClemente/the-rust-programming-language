51048de1fe1a38259ac9c5f1812c1950ee14c311
OscarClemente <oscar.clemente@gmail.com>
2021-01-19 17:54:11 +0100

    Chapter 19: Unsafe rust.

---
+++ 19-advanced_features/unsafe_rust/src/main.rs
@@ -0,0 +1,19 @@
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+fn main() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+}
