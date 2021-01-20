b1daff5b2f7d81dfa68fa469b21716bd9fffd14d
OscarClemente <oscar.clemente@gmail.com>
2021-01-20 18:10:05 +0100

    Chapter 19: Advanced Traits

---
+++ 19-advanced_features/operator_overloading/src/main.rs
@@ -0,0 +1,51 @@
+use std::ops::Add;
+
+// Overload add for self type
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+// Overload add for another type
+#[derive(Debug)]
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    let car_length = Meters{
+        0: 5,
+    };
+
+    let bumper_length = Millimeters{
+        0: 56,
+    };
+
+    let sum_size = bumper_length + car_length;
+
+    println!("{:?}", sum_size);
+}
